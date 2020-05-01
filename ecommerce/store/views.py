d17853326e481728ce45bd26f93e66d69dd7f5e6
divanov11 <ivanovsin11@gmail.com>
2020-04-30 17:30:36 -0700

    First commit

---
+++ ecommerce/store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def store(request):
+	context = {}
+	return render(request, 'store/store.html', context)
+
+def cart(request):
+	context = {}
+	return render(request, 'store/cart.html', context)
+
+def checkout(request):
+	context = {}
+	return render(request, 'store/checkout.html', context)
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\denni\onedrive\desktop\ecom_files\ecom_mod_1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
